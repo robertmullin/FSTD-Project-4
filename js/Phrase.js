4808550f989157613f1e4eda59af83714c465e19
robertmullin <17344480+robertmullin@users.noreply.github.com>
2024-04-12 19:11:44 -0400

    added project files
    and began writing the Phrase.js file

---
+++ js/Phrase.js
@@ -0,0 +1,40 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Phrase.js */
+
+// create the Phrase class
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+
+    }
+    // add letter placeholders to the display when the game starts
+    addPhraseToDisplay() {
+        const phraseList = document.getElementById("phrase").querySelector("ul");
+        for (let i=0; i < this.phrase.length; i++) {
+            const character = this.phrase[i];
+            const listItem = document.createElement("li");
+            if (character === " ") {
+                listItem.classList.add('space');
+            } else {
+                listItem.classList.add('hide', 'letter', character);
+                listItem.textContent = character;
+            }
+
+            phraseList.appendChild(listItem);
+        } 
+    } 
+    // check to see if the letter selected by the player matches a letter in the phrase
+    checkLetter(letter) {
+        const phraseArray = this.phrase.split("");
+        return phraseArray.includes(letter); 
+      }
+    // reveal the letters on the board that matches the player's selections
+    showMatchedLetter(letter) {
+        const matchedLetters = document.getElementsByClassName(letter);
+        for (let i = 0; i < matchedLetters.length; i++) {
+            matchedLetters[i].classList.remove("hide");
+            matchedLetters[i].classList.add('show');
+        }
+    }
+}
